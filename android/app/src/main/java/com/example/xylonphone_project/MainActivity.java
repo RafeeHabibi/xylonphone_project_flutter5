22c434132effcc55616bc2fdfc49415c59af5be4
Rafiullah Rahimi <rafiullah.rahimi23.com>
2024-04-03 00:09:13 -0700

    Initial commit

---
+++ android/app/src/main/java/com/example/xylonphone_project/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.xylonphone_project;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:audioplayers/audioplayers.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(XylophoneApp());
+
+class XylophoneApp extends StatelessWidget {
+  void playSound(int soundNumber) {
+    final player = AudioPlayer();
+    player.play(AssetSource('note$soundNumber.wav'));
+  }
+
+  Expanded createKey({required Color color, required int soundNumber}) {
+    return Expanded(
+      child: TextButton(
+        child: Container(color: color),
+        style: TextButton.styleFrom(padding: EdgeInsets.zero),
+        onPressed: () {
+          playSound(soundNumber);
+        },
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              createKey(color: Colors.red, soundNumber: 1),
+              createKey(color: Colors.orange, soundNumber: 2),
+              createKey(color: Colors.yellow, soundNumber: 3),
+              createKey(color: Colors.green, soundNumber: 4),
+              createKey(color: Colors.teal, soundNumber: 5),
+              createKey(color: Colors.blue, soundNumber: 6),
+              createKey(color: Colors.purple, soundNumber: 7),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
